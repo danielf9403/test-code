ab8dee5bff217ccade16d2bd994f7f90a42a1434
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-04-22 22:08:18 -0400

    carousel sliding

---
+++ frontend-test/carousel-slide/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import CarouselFadeExample from './pags/carrusel.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <CarouselFadeExample/>
+  );
+}
+
+export default App;
